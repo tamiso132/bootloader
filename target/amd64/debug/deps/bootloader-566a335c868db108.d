27888830363657627286dbf595e9dc5128055065
tamiso132 <tomasxrandi@hotmail.se>
2022-09-21 18:34:37 +0200

    hey

--- target/amd64/debug/deps/bootloader-566a335c868db108.d
+++
@@ -1,7 +0,0 @@
-/home/tamiso/projects/bootloader/target/amd64/debug/deps/bootloader-566a335c868db108: src/main.rs src/asm/mod.rs src/asm/boot.s
-
-/home/tamiso/projects/bootloader/target/amd64/debug/deps/bootloader-566a335c868db108.d: src/main.rs src/asm/mod.rs src/asm/boot.s
-
-src/main.rs:
-src/asm/mod.rs:
-src/asm/boot.s:
